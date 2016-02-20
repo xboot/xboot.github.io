da1d2fcf5b33a6f0dcd26d8c5e00a8e246331415
jianjun jiang <8192542@qq.com>
2016-02-20 13:47:59 +0800

    fix jekyll server

--- jekyll-server.sh
+++ jekyll-server.sh
@@ -2,9 +2,8 @@
 #
 # sudo apt-get install rubygems
 # sudo gem install jekyll
-# sudo gem install redcarpet
 #
 
 # Start jekyll server => Now browse to http://localhost:4000
 WEBSITE=$(cd `dirname $0` ; pwd)
-jekyll serve --watch --source ${WEBSITE} --destination ${WEBSITE}/_site
+jekyll --server 4000 ${WEBSITE} ${WEBSITE}/_site
